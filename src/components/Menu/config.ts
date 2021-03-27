839e00bedb9885a47e803f64acba446bc5db5764
PEPS <36762037+pepegoldcoin@users.noreply.github.com>
2021-03-28 00:18:14 +0530

    Sync Menu with Farms

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -4,49 +4,85 @@ const config: MenuEntry[] = [
   {
     label: 'Home',
     icon: 'HomeIcon',
-    href: 'https://www.bitblocks.finance/'
+    href: "https://swap.peps.today/",
   },
   {
-    label: 'Trade',
+    label: "PEPSwap AMM",
+    icon: 'ExchangeIcon',
+    href: "/swap",
+  },
+  {
+    label: "Liquidity",
+    icon: 'LiquidityIcon',
+    href: "/pool",
+  },
+  {
+    label: 'Buy PEPR',
+    icon: 'InfoIcon',
+    href: '/swap?outputCurrency=0x019be1796178516e060072004f267b59a49a0801',
+  },
+  {
+    label: 'Pepper Farms',
+    icon: 'FarmIcon',
+    href: 'https://swap.peps.today/',
+  },
+  {
+    label: 'Green House',
+    icon: 'PoolIcon',
+    href: 'https://swap.peps.today/',
+  },
+  {
+    label: 'Lottery (Coming Soon)',
+    icon: 'TicketIcon',
+    href: '/',
+  },
+  {
+    label: 'NFTs (Coming Soon)',
+    icon: 'NftIcon',
+    href: '/',
+  },
+    {
+    label: 'Information',
     icon: 'TradeIcon',
-    initialOpenState: true,
     items: [
       {
-        label: 'Exchange',
-        href: '/swap'
+        label: 'Github',
+        href: 'https://github.com/PEPS-Project',
       },
       {
-        label: 'Liquidity',
-        href: '/pool'
-      }
-    ]
+        label: 'DappRadar (Stats)',
+        href: 'https://dappradar.com/binance-smart-chain/defi/pepper-finance',
+      },
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0x019be1796178516e060072004f267b59a49a0801',
+      },
+      {
+        label: 'PEPR MasterChef',
+        href: 'https://bscscan.com/address/0xdfc263b5dd1703a44e5a15f2d65b6062919ba74d',
+      },
+    ],
   },
   {
-    label: 'Farms',
-    icon: 'FarmIcon',
-    href: 'https://www.bitblocks.finance/farms'
+    label: 'Tokenomics',
+    icon: 'GitbookIcon',
+    href: 'https://docs.peps.today/',
   },
-  // {
-  //   label: 'Lottery',
-  //   icon: 'TicketIcon',
-  //   href: 'https://www.BitBlocksdefi.com/lottery'
-  // },
-
   {
-    label: 'More',
+    label: 'Eco System',
     icon: 'MoreIcon',
     items: [
       {
-        label: "Github",
-        href: "https://github.com/BitBlocksproject/",
+        label: "PEPShip Store",
+        href: "https://www.pepship.com/",
       },
       {
-        label: "Docs",
-        href: "https://BitBlocksdefi.gitbook.io/bitblocks-finance/",
+        label: "Project PEPS",
+        href: "https://project.peps.today/",
       },
-
     ],
   },
+
 ]
 
 export default config
